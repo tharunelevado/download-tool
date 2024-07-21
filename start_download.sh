704ed90634d528af82f68159b918f207b7a737c2
tharun <113346953+tharunelevado@users.noreply.github.com>
2024-07-21 21:34:09 +0530

    Update start_download.sh

--- start_download.sh
+++ start_download.sh
@@ -2,7 +2,7 @@
 
 DOWNLOAD_LIST="downloads.txt"  # The file containing the list of URLs to download
 LOG_FILE="download.log"
-DOWNLOAD_DIR="/home/tharun09/download_tool/Downloads"  # Directory to save the downloads
+DOWNLOAD_DIR="/home/tharun09/download-tool/Downloads"  # Directory to save the downloads
 
 # Start the download using aria2c in background
 nohup aria2c -i "$DOWNLOAD_LIST" -d "$DOWNLOAD_DIR" -l "$LOG_FILE" > /dev/null 2>&1 &
